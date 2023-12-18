3e93b545f6c84d0c567e863168ce87f30d34de64
pkijania <139795239+pkijania@users.noreply.github.com>
2023-12-18 22:40:26 +0100

    Updated get_bitcoin_price.py

---
+++ API_Kraken/get_bitcoin_price.py
@@ -0,0 +1,15 @@
+import requests
+import datetime
+
+try:
+    get_request = requests.get('https://api.kraken.com/0/public/Ticker?pair=XBTUSD')
+    price = get_request.json()['result']['XXBTZUSD']['a'][0]
+    date = datetime.datetime.now().strftime("%c")
+    value = (price + ' USD, ' + date)
+    print(value)
+except Exception as e:
+    raise Exception(f"An error has occured due to: {e}")
+
+with open("D:/Programy/Github/Aplications/API_Kraken/bitcoin_price.txt", "a") as file:
+    file.write('\n' + repr(price) + ' USD, ' + repr(date))
+    file.close()
