9637a0bc858c63a8d27206f6e745fd6aff6e6baf
pkijania <139795239+pkijania@users.noreply.github.com>
2023-12-18 17:27:04 +0100

    Update get_bitcon_value.py

--- API_Kraken/get_bitcon_value.py
+++ API_Kraken/get_bitcon_value.py
@@ -1,7 +1,10 @@
 import requests
+import datetime
 
 get_request = requests.get('https://api.kraken.com/0/public/Ticker?pair=XBTUSD')
-if get_request is None:
-    raise Exception(f"An error has occurred")
-print("Status code is:", get_request)
-print(get_request.json())
+price = get_request.json()['result']['XXBTZUSD']['a'][0]
+date = datetime.datetime.now().strftime("%c")
+try:
+    print("Price of bitcon for:", date, "is", price)
+except:
+    print("An error has occured")
