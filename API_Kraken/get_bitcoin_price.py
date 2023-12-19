917f5abf1c038faf01e5add3be6e5745cb3e2c51
pkijania <139795239+pkijania@users.noreply.github.com>
2023-12-19 22:07:25 +0100

    Update get_bitcoin_price.py

--- API_Kraken/get_bitcoin_price.py
+++ API_Kraken/get_bitcoin_price.py
@@ -1,14 +1,20 @@
 import requests
 import datetime
+import click
 
-try:
-    get_request = requests.get('https://api.kraken.com/0/public/Ticker?pair=XBTUSD')
-    price = get_request.json()['result']['XXBTZUSD']['a'][0]
-    date = datetime.datetime.now().strftime("%c")
-    value = (price + ' USD, ' + date)
-    print(value)
-except Exception as e:
-    raise Exception(f"An error has occured due to: {e}")
+@click.command()
+@click.option('--data_file', help = 'Path to datafile.')
+def get_price(data_file):
+    try:
+        get_request = requests.get('https://api.kraken.com/0/public/Ticker?pair=XBTUSD')
+        price = get_request.json()['result']['XXBTZUSD']['a'][0]
+        date = datetime.datetime.now().strftime('%c')
+        value = (price + ' USD, ' + date)
+        print(value)
+    except Exception as e:
+        raise Exception(f'An error has occured due to: {e}')
+    with open(data_file, 'a') as file:
+        file.write('\n' + repr(price) + ' USD, ' + repr(date))
 
-with open("D:/Programy/Github/Aplications/API_Kraken/bitcoin_price.txt", "a") as file:
-    file.write('\n' + repr(price) + ' USD, ' + repr(date))
+if __name__ == 'main':
+    get_price()
