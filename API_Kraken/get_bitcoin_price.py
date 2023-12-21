f88397ae726f0a2833a4446065ee60761001b3df
pkijania <139795239+pkijania@users.noreply.github.com>
2023-12-21 19:46:03 +0100

    Updated get_bitcoin_price.py

--- API_Kraken/get_bitcoin_price.py
+++ API_Kraken/get_bitcoin_price.py
@@ -1,20 +1,29 @@
 import requests
 import datetime
 import click
+import time
 
 @click.command()
 @click.option('--data_file', help = 'Path to datafile.')
 def get_price(data_file):
-    try:
-        get_request = requests.get('https://api.kraken.com/0/public/Ticker?pair=XBTUSD')
-        price = get_request.json()['result']['XXBTZUSD']['a'][0]
-        date = datetime.datetime.now().strftime('%c')
-        value = (price + ' USD, ' + date)
-        print(value)
-    except Exception as e:
-        raise Exception(f'An error has occured due to: {e}')
     with open(data_file, 'a') as file:
-        file.write('\n' + repr(price) + ' USD, ' + repr(date))
+        file.seek(0)
+        file.truncate()
+        file.write("Bitcoin prices for: " + datetime.datetime.now().strftime('%x'))
+    while True:
+        try:
+            get_request = requests.get('https://api.kraken.com/0/public/Ticker?pair=XBTUSD')
+            price = get_request.json()['result']['XXBTZUSD']['a'][0]
+            date = datetime.datetime.now().strftime('%c')
+            value = (price + ' USD ')
+            print("Bitcoin price for: " + date + " is: ")
+            print(value)
+            print("Waiting 5 seconds for next fetch.\n")
+        except Exception as e:
+            raise Exception(f'An error has occured due to: {e}')
+        with open(data_file, 'a') as file:
+            file.write('\n' + repr(price) + ' USD, ' + repr(date))
+        time.sleep(5)
 
 if __name__ == '__main__':
     get_price()
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 This repository consists of simple aplications written in python
+
+### Get price of bitcoin
+
+Print the current price of bitcoin and paste it into "bitcoin_price.txt" file
+
+[Solution](API_KRAKEN/get_bitcoin_price.py)
