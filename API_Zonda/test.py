1bd0ba2e480c8704bde8e23a1fc9016242e829b2
pkijania <139795239+pkijania@users.noreply.github.com>
2024-01-05 14:25:39 +0100

    Updated test.py

--- API_Zonda/test.py
+++ API_Zonda/test.py
@@ -1,5 +1,11 @@
 import pytest, math
+from calculate_ema import EmaCalculcator
 
 @pytest.fixture()
-def test_ema(ema):
-    assert math.fabs(ema - 172500) < 0.01, "Assertion failed"
+def test_ema():
+    prices = [100, 105, 110, 115, 120, 125, 130, 135, 140, 145]
+    ema_test = EmaCalculcator(26)
+    for price in prices: 
+        ema_test.recalculate(price)
+    ema = ema_test.get_ema()
+    assert math.fabs(ema - 130.487) < 0.01, "Assertion failed"
