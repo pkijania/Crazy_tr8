b941e648093c10b1a1691eeb0af9ed2aec4e8082
pkijania <139795239+pkijania@users.noreply.github.com>
2024-03-24 19:59:57 +0100

    modified postgre.py

--- data/postgres.py
+++ data/postgres.py
@@ -5,15 +5,15 @@ class PostgresDataBase(DataManager):
     def __init__(self):
         print("Transfering data to postgres data base")
         self.conn = psycopg2.connect(host = "localhost", dbname = "postgres", user = "postgres", password = "toster1111", port = 5432)
-        self.cur = self.conn.cursor()
+        self.conn.set_session(autocommit=True)
 
     def insert(self, price, date, ema_long, ema_short, macd, rsi, adx):
         insert_values = 'INSERT INTO crazy_tr8 (price, date, ema_long, ema_short, macd, rsi, adx) VALUES (%s, %s, %s, %s, %s, %s, %s)'
         values = [price, date, ema_long, ema_short, macd, rsi, adx]
         with self.conn.cursor() as cur:
-            self.cur.execute(insert_values, values)
+            cur.execute(insert_values, values)
     
     def clean(self):
         clean_table = 'DELETE FROM crazy_tr8'
         with self.conn.cursor() as cur:
-            self.cur.execute(clean_table)
+            cur.execute(clean_table)
