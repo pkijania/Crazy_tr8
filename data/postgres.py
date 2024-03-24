d82ab41bf8040038230adbad0acd49b7be1e88d9
pkijania <139795239+pkijania@users.noreply.github.com>
2024-03-24 14:21:14 +0100

    modified postgres.py

--- data/postgres.py
+++ data/postgres.py
@@ -10,10 +10,10 @@ class PostgresDataBase(DataManager):
     def insert(self, price, date, ema_long, ema_short, macd, rsi, adx):
         insert_values = 'INSERT INTO crazy_tr8 (price, date, ema_long, ema_short, macd, rsi, adx) VALUES (%s, %s, %s, %s, %s, %s, %s)'
         values = [price, date, ema_long, ema_short, macd, rsi, adx]
-        self.cur.execute(insert_values, values)
-        self.conn.commit()
+        with self.conn.cursor() as cur:
+            self.cur.execute(insert_values, values)
     
     def clean(self):
         clean_table = 'DELETE FROM crazy_tr8'
-        self.cur.execute(clean_table)
-        self.conn.commit()
+        with self.conn.cursor() as cur:
+            self.cur.execute(clean_table)
