249d32fccdcd885a22caeba108a3fd12951ff04d
pkijania <139795239+pkijania@users.noreply.github.com>
2024-03-28 21:24:36 +0100

    postgres.py

---
+++ data/create_schema.sql
@@ -0,0 +1,14 @@
+-- Table: history
+
+-- DROP TABLE IF EXISTS history;
+
+CREATE TABLE IF NOT EXISTS history
+(
+    date character varying(50) primary key,
+	price character varying(50),
+    ema_long character varying(50),
+    ema_short character varying(50),
+    macd character varying(50),
+    rsi character varying(50),
+    adx character varying(50)
+)
