ff3096ea110e16135b8cadc69e1dca9ad239f692
pkijania <139795239+pkijania@users.noreply.github.com>
2024-04-03 14:22:08 +0200

    Improved formatting

--- terminal.py
+++ terminal.py
@@ -1,9 +1,10 @@
 # Module prints all the information regarding Bitcoin trading in a terminal
 
-import datetime, logging
+import datetime
+import logging
 
 class Terminal:
-    """Class providing "show_info" method for showing data in a terminal"""
+    """Class providing "time_and_date", "indicators" and "decision" methods for showing data in a terminal"""
     def __init__(self, break_time, price, ema_long, ema_short, macd, rsi, adx, buy, sell):
         """Initialize class variables"""
         self.break_time = break_time
@@ -16,18 +17,23 @@ class Terminal:
         self.buy = buy
         self.sell = sell
 
-    def show_info(self):
-        """Show: current price of Bitcoin in USD, current date, 
-        indicators and a decision whether to buy or sell crpytocurrency"""
+    def time_and_date(self):
+        """Print current price of Bitcoin in USD and current date"""
         logging.basicConfig(level=logging.INFO, format='%(message)s')
         date = datetime.datetime.now().strftime('%c')
         logging.info("Price of Bitcoin for: %s is: %s USD", date, self.price)
+    
+    def indicators(self):
+        """Print all indicators"""
         logging.info("\nIndicators:")
         logging.info("Ema for 26 days equals: %s", self.ema_long)
         logging.info("Ema for 12 days equals: %s", self.ema_short)
         logging.info("Macd equals: %s", self.macd)
         logging.info("Rsi equals: %s", self.rsi)
         logging.info("Adx equals: %s", self.adx)
-        logging.info("Buy") if self.buy == True else logging.info("Don't buy")
-        logging.info("Sell") if self.sell == True else logging.info("Don't sell")
+    
+    def decision(self):
+        """Print a decision whether to buy or sell crpytocurrency"""
+        logging.info("Buy") if self.buy else logging.info("Don't buy")
+        logging.info("Sell") if self.sell else logging.info("Don't sell")
         logging.info("\nWaiting %s seconds for next fetch.\n", str(self.break_time))
