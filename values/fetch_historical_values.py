1550a5f507aecb984b95411fe1edc17592dc4a70
pkijania <139795239+pkijania@users.noreply.github.com>
2024-04-21 22:01:55 +0200

    Update fetch_historical_values.py

--- values/fetch_historical_values.py
+++ values/fetch_historical_values.py
@@ -3,7 +3,7 @@ import csv
 class HistoricalValueFetcher:
     def __init__(self):
         list_of_values = []
-        path = "D:/Programy/Github/Crazy_tr8/historical_data/XBTUSD_Q3.csv"
+        path = "Github/Crazy_tr8/historical_data/XBTUSD_Q3.csv"
         with open(path, 'r') as file_csv:
             csv_reader = csv.reader(file_csv)
             for self.row in csv_reader:
